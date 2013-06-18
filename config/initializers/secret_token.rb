8e0da9cf34f43160ea880a6d529fbbfb06b79f37
MakerSquare Student <student@lab-7.att.net>
2013-06-18 14:04:52 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideator::Application.config.secret_token = '8ca6b334d5bc384fa4bbb8a310000d18d68af92cea46efd116ac0ac4fa3d0b97bee4110cf7681bde4a9eb8bcf376be22518d9c79767af80a220c7abb1ea9de67'
